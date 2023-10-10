97de5c35f286d01093ef8bf935b6a50593628469
David De Castro <segphault@gmail.com>
2023-10-10 19:34:16 +0200

    Ajout d'un nom à la route de redirection

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
 
     Route::get('', function () {
        return to_route('admin.property.index');
-    });
+    })->name('redirect');
     Route::resource('property', \App\Http\Controllers\Admin\PropertyController::class)->except(['show']);
 
     Route::resource('option', \App\Http\Controllers\Admin\OptionController::class)->except(['show']);
