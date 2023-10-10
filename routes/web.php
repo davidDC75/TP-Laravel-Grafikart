5bd0b1b1c24b911d30285ea62296ae042c27ced9
David De Castro <segphault@gmail.com>
2023-10-10 19:28:21 +0200

    Gestion du formulaire de contact et envoie de mail de test via maildog ou maildev

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PropertyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,12 +19,21 @@ $slugRegex = '[a-z0-9\-]+';
 
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
 
-Route::get('/biens', [\App\Http\Controllers\PropertyController::class, 'index'])->name('property.index');
+Route::get('/biens', [PropertyController::class, 'index'])
+    ->name('property.index');
 
-Route::get('/biens/{slug}-{property}', [\App\Http\Controllers\PropertyController::class, 'show'])->name('property.show')->where([
-    'property' => $idRegex,
-    'slug' => $slugRegex
+Route::get('/biens/{slug}-{property}', [PropertyController::class, 'show'])
+    ->name('property.show')
+    ->where([
+        'property' => $idRegex,
+        'slug' => $slugRegex
 ]);
+
+Route::post('/biens/{property}/contact',[PropertyController::class,'contact'])
+    ->name('property.contact')
+    ->where([
+       'property' => $idRegex
+    ]);
 
 
 Route::prefix('admin')->name('admin.')->group(function () {
